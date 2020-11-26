d8d952d3db6db6dbc48dff8309e121688db26724
Peter Marcus Hoveling <42763743+201508876PMH@users.noreply.github.com>
2020-11-26 13:17:48 +0100

    Update meshLogger_04.sh

--- meshLogger_04.sh
+++ meshLogger_04.sh
@@ -2,13 +2,19 @@ timestamp(){
  echo $(date "+%Y/%m/%d-%H:%M:%S") #current time
 }
 
+getIPAddr(){
+ echo $(ifconfig | grep -E -o 'inet addr:10.1.0.\d')
+}
+
 counter=0
 while true
 	do
 	sleep 1
 	timevariable=$(timestamp)
+	IPAddr=$(getIPAddr)
 	counter=$(($counter+1))
-	echo "Current timestamp: " $timevariable > logfile_node04.txt
+	echo "IP.addr: " $IPAddr > logfile_node04.txt
+	echo "Current timestamp: " $timevariable >> logfile_node04.txt
 	echo "Number of file iterations: " $counter >> logfile_node04.txt
         echo "" >> logfile_node04.txt
 	meshmerize neighbor >> logfile_node04.txt
